087613516183213e47aac45ef9518ad6ec8db2aa
Ashiqur Rahman Zeeshan <zeeshan.nvmx@gmail.com>
2024-12-01 23:46:32 +0600

    stall fixes

--- utils/authorize-authenticate.js
+++ utils/authorize-authenticate.js
@@ -11,8 +11,6 @@ async function authenticateUser(req, res, next) {
     const token = authHeader.split(' ')[1]
     const verifiedToken = await verifyToken(token)
 
-    console.log(verifiedToken)
-
     if (verifiedToken.customerType === 'online') {
       return res.status(401).json({ message: 'Invalid access token for this route' })
     }
